7245c2d0bfd5339e8dc7cf3fcf7209aff84dc4ca
xeelix <daelosd@gmail.com>
2025-03-18 16:46:48 +0700

    refactor: rename backend to mega-calculator and update JWT access token description

--- backend/src/auth/dto/login-response.dto.ts
+++ backend/src/auth/dto/login-response.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginResponseDto {
   @ApiProperty({
-    description: 'JWT access token 12321',
+    description: 'JWT access token',
     example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
   })
   access_token: string;
